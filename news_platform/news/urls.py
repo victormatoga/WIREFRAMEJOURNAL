edf569939b56e8003c55e60896bb9d4d887b9cb6
Victor_Matoga <victormatoga67@gmail.com>
2026-09-08 10:48:06 +0200

    Fix syntax and restore standard URL configurations

--- news_platform/news/urls.py
+++ news_platform/news/urls.py
@@ -1,10 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic.base import RedirectView
-from django.templatetags.static import static
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('news.urls')),
-    path('favicon.ico', RedirectView.as_view(url=static('images/logo.png'))),
+    path('', views.index, name='index'),
 ]
