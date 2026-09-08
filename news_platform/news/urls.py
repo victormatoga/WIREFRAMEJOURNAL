4f08986c6460de86d1aaa9294df5affd997d4646
Victor_Matoga <victormatoga67@gmail.com>
2026-09-08 10:27:02 +0200

    Fix browser favicon caching and add direct icon route

--- news_platform/news/urls.py
+++ news_platform/news/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic.base import RedirectView
+from django.templatetags.static import static
 
 urlpatterns = [
-    path('', views.article_list, name='article_list'),
-    path('article/<int:pk>/', views.article_detail, name='article_detail'),  # Changed to pk (integer ID)
-    # OR if you use slugs:
-    # path('article/<slug:slug>/', views.article_detail, name='article_detail'),
+    path('admin/', admin.site.urls),
+    path('', include('news.urls')),
+    path('favicon.ico', RedirectView.as_view(url=static('images/logo.png'))),
 ]
