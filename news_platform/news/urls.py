9fc8483c3247377be28d1c3a29a8c4a41494f79c
Victor_Matoga <victormatoga67@gmail.com>
2026-09-08 11:51:08 +0200

    Update template paths and namespace urls

--- news_platform/news/urls.py
+++ news_platform/news/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'news'  
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('article/<int:pk>/', views.article_detail, name='article_detail'),
+    # ... your other url patterns
 ]
-app_name = 'news'
